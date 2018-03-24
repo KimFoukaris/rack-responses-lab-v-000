b3b02b9d7bccd5455145cde37f41fabc8284f529
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-24 10:06:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
       resp = Rack::Response.new
-      time = Time.local
+      time = Time.localtime.now
       resp.write "#{time}"
       if time.hour < 12
         resp.write "Good Morning!"
